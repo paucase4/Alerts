c41269c7c85d75fa8e582186b3d9c83e92f6fc47
Cpau4 <paucase4@gmail.com>
2021-08-06 15:18:29 +0200

    All stored data -> class
    Client() will replace the dictionary to achieve a better and cleaner code.

---
+++ Client.py
@@ -0,0 +1,10 @@
+
+
+class Client():
+    def __init__(self,name,email,signup_date,tickers,notifications):
+        self.name = name
+        self.email = email
+        self.signup_date = signup_date
+        self.tickers = tickers
+        self.notifications = notifications
+        
