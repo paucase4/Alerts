aa64c1e723fe27692f3f5f9fdadc73d0f46d7e82
Cpau4 <paucase4@gmail.com>
2021-09-05 17:54:48 -0400

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,11 @@
+import sys
+try:
+    raise NotImplementedError("Not implemented")
+except Exception as e:
+    exception_type, exception_object, exception_traceback = sys.exc_info()
+    filename = exception_traceback.tb_frame.f_code.co_filename
+    line_number = exception_traceback.tb_lineno
+
+    print("Exception type: ", exception_type)
+    print("File name: ", filename)
+    print("Line number: ", line_number)
